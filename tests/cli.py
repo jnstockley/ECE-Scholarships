c7ae058ac5e49b326e9ff7a6029d4804472ccb8d
Brandon Egger <brandonegger64@gmail.com>
2023-02-16 13:48:01 -0600

    added separate tests for playwright and pyunit in cli

--- tests/cli.py
+++ tests/cli.py
@@ -30,12 +30,28 @@ def run(test: str = 'all'):
         streamlit_process = subprocess.Popen(streamlit_cmd, stdout=subprocess.PIPE, shell=True)
 
         time.sleep(4)
-        subprocess.run(CMD['PLAYWRIGHT'], check=False, shell=True)
+        subprocess.run(f"poetry run {CMD['PLAYWRIGHT']}", check=False, shell=True)
 
         streamlit_process.terminate()
 
         print('COVERAGE REPORT:')
         subprocess.run(CMD['REPORT'], check= False, shell=True)
+    elif test == 'playwright':
+        print('Launcing Streamlit server')
+        streamlit_cmd =f"poetry run {CMD['STREAMLIT_RUN']}"
+        # pylint: disable-next=consider-using-with
+        streamlit_process = subprocess.Popen(streamlit_cmd, stdout=subprocess.PIPE, shell=True)
+
+        time.sleep(4)
+        subprocess.run(f"poetry run {CMD['PLAYWRIGHT']}", check=False, shell=True)
+
+        streamlit_process.terminate()
+    elif test == 'pyunit':
+        print('Running PyUnit tests:')
+        poetry_pyunit_cmd =f"poetry run python -m {CMD['PYUNIT']}"
+        subprocess.run(poetry_pyunit_cmd, check=False, shell=True)
+    else:
+        typer.echo('Invalid option, please pick from the following: [all/playwright/pyunit]')
 
 if __name__ == "__main__":
     app(prog_name='cli')
