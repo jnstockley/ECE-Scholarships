38ddcb99273a3f0c6495a0bd1da6abdc770fcd28
Brandon Egger <brandonegger64@gmail.com>
2023-02-17 08:54:12 -0600

    added form wrapper for file import

--- src/pages/import.py
+++ src/pages/import.py
@@ -6,7 +6,8 @@ import streamlit as st
 from src.utils.html import centered_text
 
 st.title("Import Data")
-st.write("Add files below, when finished press import to and the system will attempt to merge your files.")
-st.file_uploader('')
-st.markdown(centered_text('<em>supports both excel and csv format</em>'), unsafe_allow_html=True)
-st.button('import')
+st.write("Add files to be imported. Multiple files can be selected and the application will attempt to merge them.")
+form = st.form(key="annotation")
+form.file_uploader('Data Sources:')
+form.markdown(centered_text('<em>supports both excel and csv format</em>'), unsafe_allow_html=True)
+form.form_submit_button('Import')
