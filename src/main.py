96a3e2443cd7d5aea3038524b6a46149f5653918
Brandon Egger <brandonegger64@gmail.com>
2023-02-18 00:02:03 -0600

    fix lint error

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 Main entry point for streamlit app.
 '''
 from st_pages import Page, show_pages
-import streamlit as st
 
 # Specify what pages should be shown in the sidebar, and what their titles
 # and icons should be
