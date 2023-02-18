48a273c61b6e9ffb837c3c033b414864bf167721
Brandon Egger <brandonegger64@gmail.com>
2023-02-17 23:57:11 -0600

    fix errors

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-poetry run streamlit run src/home.py
+poetry run streamlit run src/main.py
--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ Main entry point for streamlit app.
 from st_pages import Page, show_pages
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
 # Specify what pages should be shown in the sidebar, and what their titles
 # and icons should be
 show_pages(
