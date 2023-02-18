410198ef0f46310b9f899d9dc92d3d3440af9a5c
Brandon Egger <brandonegger64@gmail.com>
2023-02-18 00:37:55 -0600

    fix import route and headless test

--- tests/cli.py
+++ tests/cli.py
@@ -7,7 +7,7 @@ import typer
 
 CMD = {
     'STREAMLIT_RUN': 'streamlit run src/main.py --client.showErrorDetails false --server.port 9000 --server.headless true',
-    'PLAYWRIGHT': 'pytest --headed ./tests/feature/*.py',
+    'PLAYWRIGHT': 'pytest ./tests/feature/*.py',
     'PYUNIT': 'unittest discover -s tests.unit -p "*.py"',
     'REPORT': 'poetry run coverage report && poetry run coverage html'
 }
