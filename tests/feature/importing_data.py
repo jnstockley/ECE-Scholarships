9d0d9298a2c25ad69b4fee9ac6297c7a2560de0d
Brandon Egger <brandonegger64@gmail.com>
2023-02-17 22:00:27 -0600

    additional playwright example test added

--- tests/feature/importing_data.py
+++ tests/feature/importing_data.py
@@ -2,6 +2,7 @@
 Test functionality of the import document tools.
 '''
 import os
+import time
 from playwright.sync_api import Page, expect
 
 # TESTS:
@@ -22,7 +23,18 @@ def test_add_file_button(_page: Page):
     # goto import page
     # Click on add file
 
+def test_other(page: Page):
+    page.goto('https://imagecompressor.com/')
 
+    #await page.set_content("<input type=file>")
+    with page.expect_file_chooser(timeout=5000) as fc_info:
+        print('other')
+        page.get_by_text("Upload Files", exact=True).click()
+        print('click2')
+    print('bleep boop')
+    file_chooser = fc_info.value
+    assert file_chooser
+    print('test')
 
 def test_add_files(page: Page):
     '''
@@ -35,8 +47,13 @@ def test_add_files(page: Page):
     expect(add_files_btn).to_have_text('Browse files')
 
     #add_files_btn.set_input_files([os.path.join(os.getcwd(), 'tests/data/ece_scholarship_applicants.xlsx')])
-    add_files_btn.click()
+    with page.expect_file_chooser(timeout=10000) as fc_info:
+        print('event')
+        page.get_by_role("button", name="Browse files").click()
+        page.get_by_role("button", name="Browse files").click()
+        print('click1')
+    file_chooser = fc_info.value
+    print('hello')
+    #file_chooser.set_files(os.path.join(os.getcwd(), 'tests/data/ece_scholarship_applicants.xlsx'))
 
-    file_chooser = page.wait_for_event('filechooser')
-
-    file_chooser.set_files(os.path.join(os.getcwd(), 'tests/data/ece_scholarship_applicants.xlsx'))
+    assert file_chooser
