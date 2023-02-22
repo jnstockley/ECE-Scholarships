a440affe3d94da3851359359e4a1b107939f6113
Evan Lemker <lemkere@gmail.com>
2023-02-22 15:38:50 -0600

    Removed obselete table and reformated new table

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,21 +1,29 @@
 '''
-Homepage view.
+Home: Primary page for viewing student data, leaving reviews, and exporting selections
 '''
+
+# Packages used in code
 import streamlit as st
 import pandas as pd 
 import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
+# Default setting for Streamlit page
 st.set_page_config(layout="wide")
 
+# Accessing test data (Will need to replace with Teams support)
 data = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=15)
 
-AgGrid(data)
-
+st.title("Home")
+st.header("Review Applicants")
+current_filter = st.selectbox("Which filter would you like to apply?",
+                              ("Evan's custom filter", "Scholarship 1", "Scholarship 2"))
+st.write("Current filter:", current_filter)
 gb = GridOptionsBuilder.from_dataframe(data)
 gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
 gb.configure_side_bar() #Add a sidebar
 gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+
 gridOptions = gb.build()
 
 grid_response = AgGrid(
@@ -26,44 +34,13 @@ grid_response = AgGrid(
     fit_columns_on_grid_load=False,
     theme='material', #Add theme color to the table
     enable_enterprise_modules=True,
-    height=350, 
+    height=700, 
     width='100%',
     reload_data=True
 )
 
 data = grid_response['data']
-selected = grid_response['selected_rows'] 
+selected = grid_response['selected_rows']
 df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-
-st.title('Review Applicants')
-
-current_filter = st.selectbox("Which filter would you like to apply?",
-                              ("Evan's custom filter", "Scholarship 1", "Scholarship 2"))
-
-st.write("Current filter:", current_filter)
-
-st.write("***Example With Actions")
-colms = st.columns((1, 2, 1, 1, 1))
-fields = ["UID", "Name", "Program", "Sex", "Review"]
-for col, field_name in zip(colms, fields):
-    col.write(field_name)
-
-for x, uid in enumerate(data["UID"]):
-    col1, col2, col3, col4, col5 = st.columns((1, 2, 1, 1, 1))
-    col1.write(uid)
-    col2.write(data["Name"][x])
-    col3.write(data["Programs"][x])
-    col4.write(data["Sex"][x])
-    REVIEWED = False
-    BUTTON_TYPE = "Review" if not REVIEWED else "Rereview"
-    button_phold = col5.empty()  # create a placeholder
-    do_action = button_phold.button(BUTTON_TYPE, key=x)
-    if do_action:
-        # do some action with a row's data
-        button_phold.empty()  # remove button
-
-
-st.write("***Example with Built In Table")
-st.dataframe(data)
 
 st.button("Export current data")
