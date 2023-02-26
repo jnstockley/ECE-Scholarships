aaae8d1c90b3b64cbebb33558644c1bc217d8abb
Brandon Egger <beggr@uiowa.edu>
2023-02-26 02:35:19 -0600

    add conditional logic for if no files are selected

--- src/pages/import.py
+++ src/pages/import.py
@@ -8,7 +8,7 @@ from utils.html import centered_text
 st.title("Import Data")
 st.write("Add files to be imported. Multiple files can be selected and the application will attempt to merge them.")
 form = st.form(key="import_data_form")
-files = form.file_uploader('Data Sources:')
+files = form.file_uploader('Data Sources:', type=['xlsx', 'csv', 'tsv'])
 form.markdown(centered_text('<em>supports both excel and csv format</em>'), unsafe_allow_html=True)
 
 submit = form.form_submit_button('Import')
@@ -17,3 +17,8 @@ if submit:
     # Handle imported files.
     print(submit)
     print(files)
+    if files is None:
+        st.write("No files selected!")
+    else:
+        with st.container():
+            st.write("boom")
