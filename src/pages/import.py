1959a8b9e701a3f7940ade64c310bb3ea1b7cd34
Brandon Egger <beggr@uiowa.edu>
2023-02-26 02:27:24 -0600

    more logic for import file page

--- src/pages/import.py
+++ src/pages/import.py
@@ -7,7 +7,13 @@ from utils.html import centered_text
 
 st.title("Import Data")
 st.write("Add files to be imported. Multiple files can be selected and the application will attempt to merge them.")
-form = st.form(key="annotation")
-form.file_uploader('Data Sources:')
+form = st.form(key="import_data_form")
+files = form.file_uploader('Data Sources:')
 form.markdown(centered_text('<em>supports both excel and csv format</em>'), unsafe_allow_html=True)
-form.form_submit_button('Import')
+
+submit = form.form_submit_button('Import')
+
+if submit:
+    # Handle imported files.
+    print(submit)
+    print(files)
