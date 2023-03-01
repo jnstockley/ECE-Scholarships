6892fab4ce03075a235f25d456f2e07ea1dabfbf
Evan Lemker <lemkere@gmail.com>
2023-03-01 16:08:17 -0600

    Fixed functionallity of multiselect

--- src/pages/home.py
+++ src/pages/home.py
@@ -12,25 +12,33 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 st.set_page_config(layout="wide")
 
 # Accessing test data (Will need to replace with Teams support)
-data = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=15)
+df = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=15)
 
 st.title("Home")
 st.header("Review Applicants")
 current_filter = st.selectbox("Which filter would you like to apply?",
                               ("Evan's custom filter", "Scholarship 1", "Scholarship 2"))
 st.write("Current filter:", current_filter)
-gb = GridOptionsBuilder.from_dataframe(data)
-gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-gb.configure_side_bar() #Add a sidebar
-gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
 
-gridOptions = gb.build()
+# Building the AgGrid Table
+gd = GridOptionsBuilder.from_dataframe(df)
+gd.configure_pagination(enabled=True) #Add pagination
+gd.configure_side_bar() #Add a sidebar
+gd.configure_default_column(editable=True, groupable=True)
+gd.configure_selection(selection_mode='multiple', use_checkbox=True) #Enable multi-row selection
 
+
+gridoptions = gd.build()
+AgGrid(df, gridOptions=gridoptions)
+
+st.button("Export current data")
+
+'''
 grid_response = AgGrid(
     data,
     gridOptions=gridOptions,
     data_return_mode='AS_INPUT', 
-    update_mode='MODEL_CHANGED', 
+    update_mode='SELECTION_CHANGED', 
     fit_columns_on_grid_load=False,
     theme='material', #Add theme color to the table
     enable_enterprise_modules=True,
@@ -42,5 +50,4 @@ grid_response = AgGrid(
 data = grid_response['data']
 selected = grid_response['selected_rows']
 df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-
-st.button("Export current data")
+'''
