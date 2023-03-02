7c9d2ed9f98fefdc56d8b1f9181747fb08bca75f
Evan Lemker <lemkere@gmail.com>
2023-03-01 18:30:22 -0600

    Added styling capabilities for main table

--- src/pages/home.py
+++ src/pages/home.py
@@ -6,14 +6,14 @@ Home: Primary page for viewing student data, leaving reviews, and exporting sele
 import streamlit as st
 import pandas as pd 
 import numpy as np
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+from st_aggrid import GridOptionsBuilder, AgGrid, ColumnsAutoSizeMode
 from streamlit_extras.stateful_button import button
 
 # Default setting for Streamlit page
 st.set_page_config(layout="wide")
 
 # Accessing test data (Will need to replace with Teams support)
-df = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=15)
+df = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=100)
 df.insert(0, 'Selection', None)
 
 st.title("Home")
@@ -28,10 +28,19 @@ gd.configure_pagination(enabled=True) #Add pagination
 gd.configure_side_bar() #Add a sidebar
 gd.configure_default_column(editable=False, groupable=True)
 gd.configure_selection(selection_mode='multiple', use_checkbox=True) #Enable multi-row selection
-
-
 gridoptions = gd.build()
-grid_table = AgGrid(df, gridOptions=gridoptions)
+
+custom_css = {}
+
+grid_table = AgGrid(
+    df, 
+    gridOptions=gridoptions,
+    theme='balham',
+    custom_css=custom_css,
+    height = 700,
+    columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
+)
+
 sel_rows = grid_table["selected_rows"]
 st.write(sel_rows)
 
@@ -46,23 +55,3 @@ with st.container():
     with col3:
         if button('Export Selected Students', key='Delete Existing Scholarship'):
             st.write('form for deleting')
-
-
-'''
-grid_response = AgGrid(
-    data,
-    gridOptions=gridOptions,
-    data_return_mode='AS_INPUT', 
-    update_mode='SELECTION_CHANGED', 
-    fit_columns_on_grid_load=False,
-    theme='material', #Add theme color to the table
-    enable_enterprise_modules=True,
-    height=700, 
-    width='100%',
-    reload_data=True
-)
-
-data = grid_response['data']
-selected = grid_response['selected_rows']
-df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-'''
