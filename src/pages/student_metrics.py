a15525d765b6d26d4c4704b6caa475fa9e1c67bc
Ashelyn Mann <ashem@uiowa.edu>
2023-03-03 08:22:48 -0600

    finished core functionality, need to format page and mock data.

--- src/pages/student_metrics.py
+++ src/pages/student_metrics.py
@@ -5,29 +5,29 @@ import numpy as np
 
 def create_fig(data, data_title, highlight=None):
     fig, ax = plt.subplots()
-    n, bin1, patches = ax.hist(data, bins=20)
+    n, bins, patches = ax.hist(data, bins=20)
     if highlight is not None:
-        patches[5].set_fc('red')
+        patches[np.digitize([highlight],bins)[0]-1].set_fc('red')
     plt.title(data_title)
     st.pyplot(fig)
     return fig, ax
 
 
-def build_metrics_page(student_data, highlight_id=None):
+def build_metrics_page(data, highlight_id=None):
     st.title('Student Metrics')
     student_select = st.selectbox("Select Student",["temp1","temp2","temp3"])
     with st.container():
         col1, col2, col3 = st.columns(3)
         with col1:
             st.write('Graph 1')
-            create_fig(student_data['ACT'], 'ACT', student_data['ACT'][5])
-            create_fig(student_data['SAT'], 'SAT')
+            create_fig(data['ACT'], 'ACT', data['ACT'][5])
+            create_fig(data['SAT'], 'SAT')
         with col2:
             st.write('Graph 2')
-            create_fig(student_data['GPA'], 'GPA')
+            create_fig(data['GPA'], 'GPA', data['GPA'][36])
         with col3:
             st.write('Graph 3')
-            create_fig(student_data['Financial Need'], 'Financial Need')
+            create_fig(data['Financial Need'], 'Financial Need', data['Financial Need'][77])
     return
 
 #Assuming data is a dictionary
