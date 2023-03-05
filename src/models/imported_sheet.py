3e378073280393c9ea8429b4d022c74f1aefcff9
Brandon <beggr@uiowa.edu>
2023-03-04 23:16:26 -0600

    add ImportedSheet object for storing the dataframe"

---
+++ src/models/imported_sheet.py
@@ -0,0 +1,42 @@
+'''
+Imported data sheet representation
+'''
+import os
+import pandas as pd
+
+class ImportedSheet:
+    '''
+    Object represenation of 
+    '''
+    def __init__(self, path):
+        '''
+        Initialize an object given a path. Loads dataframe.
+        '''
+        self._path = path
+        self._file_name = os.path.basename(path)
+        self._load_file_into_memory()
+
+    def get_file_name(self):
+        '''
+        Getter for file path basename
+        '''
+        return self._file_name
+
+    def get_df(self):
+        '''
+        Gets the pandas dataframe
+        '''
+        return self.data
+
+    def get_path(self):
+        '''
+        Getter for file path
+        '''
+        return self._path
+
+    def _load_file_into_memory(self):
+        '''
+        Takes the file path and loads it into memory
+        Will handle whether file is csv or excel.
+        '''
+        self.data = pd.read_excel(self._path)
