355b2a314b997e91da5e23120228253de0798e88
abeschen <abeschen@uiowa.edu>
2023-03-20 14:19:39 -0500

    Edit page layout to look better.

--- src/pages/scholarship_management.py
+++ src/pages/scholarship_management.py
@@ -8,19 +8,15 @@ st.title("Scholarship Management")
 st.write("Select an Action from Below")
 
 with st.container():
-    col1, col2, col3= st.columns(3)
-    with col1:
-        if button('Create New Scholarship', key='Create New Scholarship'):
-            st.write('Create a new scholarship')
-            st.write('How many scholarships are available?')
-            st.write('How much are the scholarships worth?')
-            st.write('What are the criteria of the scholarship?')
-    with col2:
-        if button('Edit Existing Scholarship', key='Edit Existing Scholarship'):
-            st.write('form for editing')
-    with col3:
-        if button('Delete Existing Scholarship', key='Delete Existing Scholarship'):
-            st.write('form for deleting')
+    if button('Create New Scholarship', key='Create New Scholarship'):
+        st.title('Create a new scholarship')
+        st.write('How many scholarships are available?')
+        st.write('How much are the scholarships worth?')
+        st.write('What are the criteria of the scholarship?')
+    if button('Edit Existing Scholarship', key='Edit Existing Scholarship'):
+        st.write('form for editing')
+    if button('Delete Existing Scholarship', key='Delete Existing Scholarship'):
+        st.write('form for deleting')
 
 #if button("Button 1", key="button1"):
 #if button("Button 2", key="button2"):
