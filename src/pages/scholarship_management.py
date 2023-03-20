50611df52ca14cdeee70f7ce5659af67d14e8c66
abeschen <abeschen@uiowa.edu>
2023-03-20 14:33:25 -0500

    Playing around with looks.

--- src/pages/scholarship_management.py
+++ src/pages/scholarship_management.py
@@ -10,8 +10,10 @@ st.write("Select an Action from Below")
 with st.container():
     if button('Create New Scholarship', key='Create New Scholarship'):
         st.title('Create a new scholarship')
-        st.write('How many scholarships are available?')
-        st.write('How much are the scholarships worth?')
+        #Note: text_input is used instead of number_input because I feel like the stepping functionality is unnecessary and could
+        #throw them off when they see it or they could accidentally hit it. This can be changed later if it is found useful.
+        st.text_input("Amount of Scholarships", max_chars=8, placeholder="Enter Numerical Amount")
+        st.text_input('The value of each individual Scholarship', max_chars=8, placeholder="Enter Numerical Amount")
         st.write('What are the criteria of the scholarship?')
     if button('Edit Existing Scholarship', key='Edit Existing Scholarship'):
         st.write('form for editing')
