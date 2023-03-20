207271c354095cfa0094c14ebfa13852654bb681
abeschen <abeschen@uiowa.edu>
2023-03-20 14:53:01 -0500

    Some basic criteria added.

--- src/pages/scholarship_management.py
+++ src/pages/scholarship_management.py
@@ -3,6 +3,7 @@ scholarship managment page render
 '''
 import streamlit as st
 from streamlit_extras.stateful_button import button
+import numpy as np
 
 st.title("Scholarship Management")
 st.write("Select an Action from Below")
@@ -12,14 +13,12 @@ with st.container():
         st.title('Create a new scholarship')
         #Note: text_input is used instead of number_input because I feel like the stepping functionality is unnecessary and could
         #throw them off when they see it or they could accidentally hit it. This can be changed later if it is found useful.
-        st.text_input("Amount of Scholarships", max_chars=8, placeholder="Enter Numerical Amount")
-        st.text_input('The value of each individual Scholarship', max_chars=8, placeholder="Enter Numerical Amount")
-        st.write('What are the criteria of the scholarship?')
+        total = st.text_input("Total amount of Scholarships", max_chars=8, placeholder="Enter Numerical Amount")
+        value = st.text_input('The value of each individual Scholarship', max_chars=8, placeholder="Enter Numerical Amount")
+        act = st.select_slider('Select the minimum ACT requirement', options=range(1,37))
+        major = st.selectbox("Select which majors the scholarship applies to", options=["Computer Science and Engineering", "Electrical Engineering", "N/A"])
+        gpa = st.select_slider('Select the minimum GPA requirement', options=(x/5 for x in range (0,51)))
     if button('Edit Existing Scholarship', key='Edit Existing Scholarship'):
         st.write('form for editing')
     if button('Delete Existing Scholarship', key='Delete Existing Scholarship'):
         st.write('form for deleting')
-
-#if button("Button 1", key="button1"):
-#if button("Button 2", key="button2"):
-#if button("Button 3", key="button3"):
