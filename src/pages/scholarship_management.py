d8e65fa979bde10009dc53c1b7309c6d4befd279
abeschen <abeschen@uiowa.edu>
2023-03-21 00:09:47 -0500

    Some layout changes

--- src/pages/scholarship_management.py
+++ src/pages/scholarship_management.py
@@ -8,14 +8,22 @@ if 'scholarships' not in st.session_state:
     st.session_state['scholarships'] = {}
 if 'scholarship_names' not in st.session_state:
     st.session_state['scholarship_names'] = []
+if 'create_vis' not in st.session_state:
+    st.session_state['create_disabled'] = False
+if 'edit_vis' not in st.session_state:
+    st.session_state['edit_disabled'] = False
+if 'delete_vis' not in st.session_state:
+    st.session_state['delete_disabled'] = False
 
 st.title("Scholarship Management")
 st.write("Select an Action from Below")
 
 with st.container():
     majors = ["Computer Science and Engineering", "Electrical Engineering", "N/A"]
-    if button('Create New Scholarship', key='Create New Scholarship'):
+    if button('Create New Scholarship', disabled=st.session_state["create_disabled"], key='Create New Scholarship'):
         st.title('Create a new scholarship')
+        st.session_state["edit_disabled"] = True
+        st.session_state['delete_disabled'] = True
         #NOTE: text_input is used instead of number_input because I feel like the stepping functionality is unnecessary and could
         #throw them off when they see it or they could accidentally hit it. This can be changed later if it is found useful.
         name = st.text_input("Scholarship Name", max_chars=500, placeholder="Enter Scholarship Name")
@@ -26,11 +34,18 @@ with st.container():
         sat = st.select_slider('Select the minimum SAT requirement', options=(x*10 for x in range(0,161)))
         gpa = st.select_slider('Select the minimum GPA requirement', options=(x/5 for x in range (0,51)))
         if st.button('Create'):
-            scholarship_values = {'total': total, 'value': value, 'major': major, 'act': act, 'sat': sat, 'gpa': gpa}
-            st.session_state["scholarships"][name] = scholarship_values
-            st.session_state["scholarship_names"].append(name)
-    if button('Edit Existing Scholarship', key='Edit Existing Scholarship'):
+            print(name)
+            if name == "" or total == "" or value == "":
+                st.write("Please make sure all the fields are filled out.")
+            else:
+                scholarship_values = {'total': total, 'value': value, 'major': major, 'act': act, 'sat': sat, 'gpa': gpa}
+                st.session_state["scholarships"][name] = scholarship_values
+                st.session_state["scholarship_names"].append(name)
+
+    if button('Edit Existing Scholarship', disabled=st.session_state["edit_disabled"], key='Edit Existing Scholarship'):
         edit_sch = st.selectbox("Select the scholarship to edit", options=st.session_state['scholarship_names'])
+        st.session_state['create_disabled'] = True
+        st.session_state['delete_disabled'] = True
         if button('Edit This Scholarship', key = 'Edit This Scholarship'):
             if edit_sch is None:
                 st.write('There is no scholarship selected')
@@ -47,9 +62,11 @@ with st.container():
                     newValues = {'total': total, 'value': value, 'major': major, 'act': act, 'sat': sat, 'gpa': gpa}
                     st.session_state["scholarships"][name] = newValues
 
-    if button('Delete Existing Scholarship', key='Delete Existing Scholarship'):
+    if button('Delete Existing Scholarship', disabled=st.session_state["delete_disabled"], key='Delete Existing Scholarship'):
         delete_sch = st.selectbox("Select the scholarship to delete", options=st.session_state['scholarship_names'])
         if button ('Delete This Scholarship', key='Delete This Scholarship'):
+            st.session_state['create_disabled'] = True
+            st.session_state['edit_disabled'] = True
             if delete_sch is None:
                 st.write('There is no scholarship selected')
             else:
