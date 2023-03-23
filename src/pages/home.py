51e50653e73c7b2454ae3984372911e6ec83e743
Evan Lemker <lemkere@gmail.com>
2023-03-23 11:34:11 -0500

    Extracted submission method for easier testing, added error checks for recommendations

--- src/pages/home.py
+++ src/pages/home.py
@@ -13,12 +13,15 @@ from st_aggrid import JsCode, GridOptionsBuilder, AgGrid, ColumnsAutoSizeMode, G
 st.set_page_config(layout="wide")
 
 # Importing data
-students = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=100)
-scholarships = pd.read_excel("./tests/data/scholarships.xlsx")
-user_recommendations = pd.read_excel("./tests/data/Test_User_Reviews.xlsx")
+global STUDENTS
+global SCHOLARSHIPS
+global USER_RECOMMENDATIONS
+STUDENTS = pd.read_excel("./tests/data/ece_scholarship_applicants.xlsx", nrows=100)
+SCHOLARSHIPS = pd.read_excel("./tests/data/scholarships.xlsx")
+USER_RECOMMENDATIONS = pd.read_excel("./tests/data/Test_User_Reviews.xlsx")
 
 # Creating main dataframe
-students.insert(0, 'Select All', None)
+STUDENTS.insert(0, 'Select All', None)
 
 # Helper functions for JavaScript
 js = JsCode("""
@@ -29,7 +32,7 @@ js = JsCode("""
      });    
  }
  """)
-clearJs = '''<script>
+CLEARJS = '''<script>
      ((e) => {
         const iframe = window.parent.document.querySelectorAll('[title="st_aggrid.agGrid"]')[0] || null;
         if(!iframe) return;
@@ -43,10 +46,10 @@ st.title("Home")
 st.header("Review Applicants")
 
 # Filter selection (Will want to implement this once we have example filters)
-current_filter = st.selectbox("Which scholarship criteria woudld you like to filter by?", np.append(["None"], scholarships["Name"].values))
+current_filter = st.selectbox("Which scholarship criteria woudld you like to filter by?", np.append(["None"], SCHOLARSHIPS["Name"].values))
 
 # Configuring options for table functionality
-gd = GridOptionsBuilder.from_dataframe(students)
+gd = GridOptionsBuilder.from_dataframe(STUDENTS)
 gd.configure_pagination(enabled=True) #Add pagination
 gd.configure_side_bar() #Add a sidebar
 gd.configure_default_column(editable=False, groupable=True)
@@ -61,7 +64,7 @@ custom_css = {}
 
 # Building the table
 grid_table = AgGrid(
-    students,
+    STUDENTS,
     gridOptions=gridoptions,
     theme='balham',
     custom_css=custom_css,
@@ -70,9 +73,28 @@ grid_table = AgGrid(
     allow_unsafe_jscode=True,
     update_mode=GridUpdateMode.MODEL_CHANGED
 )
-
+st.write(len(grid_table["selected_rows"]))
 # Displaying statistics about main data frame
 st.write("Number of students selected: ", len([student["Name"] for student in grid_table["selected_rows"]]))
+if st.button("Clear Selection"): 
+    components.html(CLEARJS)
+
+def submit_recommendations(recommended_scholarship, additional_feedback): 
+    global USER_RECOMMENDATIONS
+    if len(grid_table["selected_rows"]) == 0:
+        return False, "Must select students to recommend"
+    sel_uids = [key["UID"] for key in grid_table["selected_rows"]]
+    new_recommendations = pd.DataFrame(columns= ['UID', 'Scholarship', 'Additional Feedback'])
+    for uid in sel_uids:
+        new_recommendation = {"UID": uid, "Scholarship": recommended_scholarship, "Additional Feedback": additional_feedback}
+        if len(USER_RECOMMENDATIONS.loc[(USER_RECOMMENDATIONS['UID'] == uid) & (USER_RECOMMENDATIONS['Scholarship'] == recommended_scholarship)]) > 0: 
+            return False, str("Already recommended student " + str(uid) + " for this scholarship")
+        # Check here if students meets requirements of scholarship (Need to wait to merge Austin's PR before these)
+        new_recommendations = new_recommendations.append(new_recommendation, ignore_index=True)
+    # Check here for it too many recommendations for that scholarship, should be none if unlimited
+    USER_RECOMMENDATIONS = USER_RECOMMENDATIONS.append(new_recommendations)
+    USER_RECOMMENDATIONS.to_excel('./tests/data/Test_User_Reviews.xlsx', index = False)
+    return True, None
 
 # Actions for user to take on main data frame
 with st.container():
@@ -80,31 +102,34 @@ with st.container():
 
     # Submitting recommendations for scholarhsips
     with col1:
-        with st.expander("Review Selected Students", expanded = False):
-            with st.form("recommendation_form", clear_on_submit=True):
-                recommended_scholarship = st.selectbox("Select Scholarship to Recommend Students For:", scholarships.Name)
+        with st.expander("Review Selected Students"):
+            with st.form("recommendation_form"):
+                recommended_scholarship = st.selectbox("Select Scholarship to Recommend Students For:", SCHOLARSHIPS.Name)
                 additional_feedback = st.text_area("Enter any additional feedback on students")
-                submit_recommendation = st.form_submit_button("Submit Recommendation")
+                submit_recommendation = st.form_submit_button("Submit Recommendation")            
                 if "success" not in st.session_state:
                     st.session_state['success'] = False
+                if "failure" not in st.session_state:
+                    st.session_state['failure'] = False
+                if "error" not in st.session_state:
+                    st.session_state['error'] = False
                 if st.session_state['success'] is True:
-                    st.success("Success")
+                    st.success("Successfuly submitted recommendations!")
+                if st.session_state['failure'] is True:
+                    st.error(st.session_state['error'])
                 if submit_recommendation:
-                    sel_uids = [key["UID"] for key in grid_table["selected_rows"]]
-                    new_recommendations = pd.DataFrame(columns= ['UID', 'Scholarship', 'Additional Feedback'])
-                    for uid in sel_uids:
-                        new_recommendation = {"UID": uid, "Scholarship": recommended_scholarship, "Additional Feedback": additional_feedback}
-                        new_recommendations = new_recommendations.append(new_recommendation, ignore_index=True)
-                    user_recommendations = user_recommendations.append(new_recommendations)
-                    user_recommendations.to_excel('./tests/data/Test_User_Reviews.xlsx', index = False)
-                    components.html(clearJs)
-                    st.session_state["success"] = True
-    
+                    result, errorMessage = submit_recommendations(recommended_scholarship, additional_feedback)
+                    if result is True:
+                        st.session_state['success'] = True
+                        st.session_state['failure'] = False
+                    else:
+                        st.session_state['success'] = False
+                        st.session_state['failure'] = True
+                        st.session_state['error'] = errorMessage
     # Viewing graphs of student distributions
     with col2:
         with st.expander("See Distribution of Selected Students"):
             st.write("Add Ashelyn's Data Analysis")
-    
     # Exporting the selected students
     with col3:
         st.button("Export Selected Students")
