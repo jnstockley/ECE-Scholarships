9013d9b03ff30d3098fa81437be033c579f26283
Ashelyn Mann <ashem@uiowa.edu>
2023-03-27 06:54:36 -0500

    fixing linting stuff that needed to be fixed

--- src/pages/student_metrics.py
+++ src/pages/student_metrics.py
@@ -1,24 +1,34 @@
+'''
+Information Graph Rendering
+'''
+import math
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
+
+'''
+Function to create pyplot figure based on input data,
+will highlight a bin if that data is inputted
+'''
 def create_fig(data, data_title, highlight=None):
     highlight = highlight.values[0]
-    fig, ax = plt.subplots()
-    n, bins, patches = ax.hist(data, bins=20)
+    fig, axis = plt.subplots()
+    _, bins, patches = axis.hist(data, bins=20)
     if highlight is not None and not math.isnan(highlight):
-        b = pd.cut([highlight],bins)
-        if type(b[0]) != float:
-            binc = np.where(abs(bins-b[0].left) < 0.001)
+        bin = pd.cut([highlight],bins)
+        if not isinstance(b[0],float):
+            binc = np.where(abs(bins-bin[0].left) < 0.001)
             binc = binc[0][0]
             patches[binc].set_fc('red')
     plt.title(data_title)
     st.pyplot(fig)
-    return fig, ax
+    return fig, axis
 
-
+'''
+Function to build the metrics page from a given dataset
+'''
 def build_metrics_page(data):
     st.title('Student Metrics')
     student_select = st.selectbox("Select Student",data['Name'])
@@ -34,7 +44,6 @@ def build_metrics_page(data):
         with col3:
             st.write('Graph 3')
             create_fig(data['High School GPA'], 'High School GPA', data['High School GPA'][highlight_id])
-    return
 
 #Assuming data is a dictionary
 
