337c2d6e20447e6519fefc159dca0821c9d2134b
Brandon Egger <brandonegger64@gmail.com>
2023-03-28 19:08:26 -0500

    fix failing graph_page tests

--- tests/feature/graph_page.py
+++ tests/feature/graph_page.py
@@ -5,22 +5,21 @@ from playwright.sync_api import Page, expect
 
 def test_graph_page(page: Page):
     '''
+    Test graph page visible
     '''
     page.goto("http://localhost:9000")
     metrics_link = page.get_by_role("link", name="Student Metrics")
 
-    expect(metrics_link).to_have_test('Student Metrics')
+    expect(metrics_link).to_have_text('Student Metrics')
 
     metrics_link.click()
     metric_data_heading = page.get_by_role("heading", name="Student Metrics").get_by_text("Student Metrics")
 
     expect(metric_data_heading).to_have_text("Student Metrics")
-    return
 
 def test_student_selectbox(page: Page):
     '''
+    Test student selectbox working
     '''
     page.goto("http://localhost:9000/Student Metrics")
-    select_student_sb = page.get_by_role("selectbox", name="Select Student")
-
-    expect(select_student_sb).to_have_text('Leyla Hair')
+    page.locator("div").filter(has_text="Leyla Hair").first.is_visible()
