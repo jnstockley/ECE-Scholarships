948edc486e86f311f952fdf66101ab8b3b68f034
Brandon Egger <brandonegger64@gmail.com>
2023-03-28 21:18:08 -0500

    add fixtures for import_data session state

---
+++ tests/feature/conftest.py
@@ -0,0 +1,6 @@
+'''
+Pytest configuration file. Can store fixtures, hooks, and other configuration options.
+'''
+pytest_plugins = [
+   "tests.feature.fixtures.import_data",
+]
--- tests/feature/importing_data.py
+++ tests/feature/importing_data.py
@@ -2,7 +2,6 @@
 Test functionality of the import document tools.
 '''
 import os
-import re
 import pytest
 from playwright.sync_api import Page, expect
 
@@ -35,9 +34,8 @@ def test_add_file_button(page: Page):
     # The page should have a browse files button
     expect(add_files_btn).to_have_text('Browse files')
 
-# File uploader with streamlit not working on chromium
-@pytest.mark.skip_browser("chromium")
-def test_import_files_start_to_end(page: Page):
+@pytest.mark.usefixtures("skip_all_similar_import_complete_page")
+def test_import_files_start_to_end(skip_all_similar_import_complete_page: Page):
     '''
     As a user so that I can upload scholarship applications details from multiple sources,
     I would like to be able to upload several excel files and combine them across a single
@@ -45,51 +43,13 @@ def test_import_files_start_to_end(page: Page):
 
     This test verifies the entire import data flow is functioning
     '''
-    page.goto("http://localhost:9000/Import%20Data", wait_until='domcontentloaded')
+    expect(skip_all_similar_import_complete_page.get_by_text("import completed!")).to_have_text("import completed!")
 
-    with page.expect_file_chooser() as fc_info:
-        page.get_by_role("button", name="Browse files").click()
-    file_chooser = fc_info.value
-    file_chooser.set_files(SAMPLE_FILE_PATHS)
+    skip_all_similar_import_complete_page.get_by_role("button", name="import another").click()
+    expect(skip_all_similar_import_complete_page.get_by_role("heading", name="Import Data")).to_have_text("Import Data")
 
-    page.get_by_role("button", name="Import").click()
-    expect(page.get_by_text("Select Alignment Columns")).to_have_text("Select Alignment Columns")
-
-    page.get_by_role("combobox", name="Selected Category. ece_scholarship_applicants.xlsx").click()
-    page.get_by_text("UID").click()
-    page.get_by_role("combobox", name="Selected Decision_Date. ece_school_applicants.xlsx").click()
-    page.get_by_text("UID.1").click()
-
-    page.get_by_role("textbox", name="Final column name:").click()
-    page.get_by_role("textbox", name="Final column name:").fill("UID")
-
-    page.get_by_role("button", name="submit").click()
-
-    expect(page.get_by_text("Duplicate Column(s) Found")).to_have_text("Duplicate Column(s) Found")
-
-    page.get_by_role("button", name="Next").click()
-    page.get_by_role("button", name="Next").click()
-
-    expect(page.get_by_text('Similar Columns Have Been Detected')).to_be_visible(timeout=20000)
-
-    remaining_label = page.get_by_role('paragraph').filter(has_text=re.compile(".+ remaining..."))
-    remaining_count = int(remaining_label.inner_text().replace(" remaining...", ""))
-    # Keep clicking skip until we have reached the end
-    for i in range(remaining_count + 1, -1, -1):
-        page.get_by_role("button", name="skip").click()
-        if not i == 0:
-            # Click it to await its presence on page
-            page.get_by_text(f"{i-1} remaining...").click()
-
-    expect(page.get_by_text("import completed!")).to_have_text("import completed!")
-
-    page.get_by_role("button", name="import another").click()
-
-    expect(page.get_by_role("heading", name="Import Data")).to_have_text("Import Data")
-
-# File uploader with streamlit not working on chromium
-@pytest.mark.skip_browser("chromium")
-def test_import_files_start_to_end_with_merging(page: Page):
+@pytest.mark.usefixtures("merge_all_similar_import_complete_page")
+def test_import_files_start_to_end_with_merging(merge_all_similar_import_complete_page: Page):
     '''
     As a user so that I can upload scholarship applications details from multiple sources,
     I would like to be able to upload several excel files and combine them across a single
@@ -97,47 +57,10 @@ def test_import_files_start_to_end_with_merging(page: Page):
 
     With merging specifies that the user chooses to merge all similar columns instead of skip!
     '''
-    page.goto("http://localhost:9000/Import%20Data", wait_until='domcontentloaded')
+    expect(merge_all_similar_import_complete_page.get_by_text("import completed!")).to_have_text("import completed!")
 
-    with page.expect_file_chooser() as fc_info:
-        page.get_by_role("button", name="Browse files").click()
-    file_chooser = fc_info.value
-    file_chooser.set_files(SAMPLE_FILE_PATHS)
-
-    page.get_by_role("button", name="Import").click()
-    expect(page.get_by_text("Select Alignment Columns")).to_have_text("Select Alignment Columns")
-
-    page.get_by_role("combobox", name="Selected Category. ece_scholarship_applicants.xlsx").click()
-    page.get_by_text("UID").click()
-    page.get_by_role("combobox", name="Selected Decision_Date. ece_school_applicants.xlsx").click()
-    page.get_by_text("UID.1").click()
-
-    page.get_by_role("textbox", name="Final column name:").click()
-    page.get_by_role("textbox", name="Final column name:").fill("UID")
-
-    page.get_by_role("button", name="submit").click()
-
-    expect(page.get_by_text("Duplicate Column(s) Found")).to_have_text("Duplicate Column(s) Found")
-
-    page.get_by_role("button", name="Next").click()
-    page.get_by_role("button", name="Next").click()
-
-    expect(page.get_by_text('Similar Columns Have Been Detected')).to_be_visible(timeout=20000)
-
-    remaining_label = page.get_by_role('paragraph').filter(has_text=re.compile(".+ remaining..."))
-    remaining_count = int(remaining_label.inner_text().replace(" remaining...", ""))
-    # Keep clicking skip until we have reached the end
-    for i in range(remaining_count + 1, -1, -1):
-        page.get_by_role("button", name="merge").click()
-        if not i == 0:
-            # Click it to await its presence on page
-            page.get_by_text(f"{i-1} remaining...").click()
-
-    expect(page.get_by_text("import completed!")).to_have_text("import completed!")
-
-    page.get_by_role("button", name="import another").click()
-
-    expect(page.get_by_role("heading", name="Import Data")).to_have_text("Import Data")
+    merge_all_similar_import_complete_page.get_by_role("button", name="import another").click()
+    expect(merge_all_similar_import_complete_page.get_by_role("heading", name="Import Data")).to_have_text("Import Data")
 
 def test_import_files_with_no_files_selected(page: Page):
     '''
