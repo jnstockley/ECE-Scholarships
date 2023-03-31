b982b88bcf31231e655d51a9a07419034a53b349
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2023-03-31 15:11:31 -0500

    feat: implemented `logged_in` function

--- src/utils/sharepoint.py
+++ src/utils/sharepoint.py
@@ -5,9 +5,18 @@ Ensure login
 Uploads files
 Downloads files
 """
+import re
+import time
+from re import Pattern
 
 import extra_streamlit_components as stx
 
+
+HAWKID_REGEX = re.compile(r"[a-zA-Z]{1}[a-zA-Z0-9]{2,}@uiowa.edu")
+
+PASSWORD_REGEX = re.compile(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&]?)[A-Za-z\d@$!%*#?&]{8,}$")
+
+SITE_URL_REGEX = re.compile(r"https://iowa.sharepoint.com/sites/.+")
 
 def get_manager():
     """
@@ -16,11 +25,37 @@ def get_manager():
     return stx.CookieManager()
 
 
+def regex_validation(string: str, regex: Pattern[str]) -> bool | None:
+    """
+    Validates regex, used to check sign in creds before connecting to sharepoint
+    :param string: String to match
+    :param regex: Regex used to match against the string
+    :return: True if matches, otherwise none
+    """
+    return re.fullmatch(regex, string)
+
+
 def logged_in() -> bool:
     """
     Checks if the cookies are present, and in a valid format
     :return: True if cookie looks good, otherwise false
     """
+    # Work around for caching not working with cookie manager
+    time.sleep(0.2)
+
+    manager = get_manager()
+    cookies = manager.get_all()
+    if "cred" not in cookies:
+        return False
+    creds = manager.get("cred")
+    if type(creds) is dict and "hawk-id" not in creds and "password" not in creds and "site-url" not in creds:
+        return False
+    hawk_id = creds['hawk-id']
+    password = creds['password']
+    site_url = creds['site-url']
+    if regex_validation(hawk_id, HAWKID_REGEX) and regex_validation(password, PASSWORD_REGEX) \
+            and regex_validation(site_url, SITE_URL_REGEX):
+        return True
     return False
 
 
