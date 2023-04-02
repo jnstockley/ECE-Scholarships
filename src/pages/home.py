08b9e54dc5ac1f55dff7b0a78d1cdf51b79f8873
Ashelyn Mann <ashem@uiowa.edu>
2023-04-02 16:59:09 -0500

    color coding of selected students works. Legend isnt working yet, graph also needs to be moved.

--- src/pages/home.py
+++ src/pages/home.py
@@ -29,24 +29,15 @@ def dynamic_fig(df, x_axis, y_axis, highlights=None):
     xs = df[x_axis][df[x_axis] != 0][df[y_axis] != 0]
     ys = df[y_axis][df[x_axis] != 0][df[y_axis] != 0]
     plt.scatter(xs, ys)
+    if highlights is not None:
+        plt.scatter(df.iloc[highlights][x_axis],df.iloc[highlights][y_axis],color='r')
+        plt.legend(df.iloc[highlights]['Name'])
     plt.xlabel(x_axis)
     plt.ylabel(y_axis)
-    #plt.legend(['<Highlighted Students>'])
     st.pyplot(fig)
     return fig, axis
 
-with st.container():
-    numeric_cols = df.copy().apply(lambda s: pd.to_numeric(s, errors='coerce').notnull().all())
-    numeric_cols = numeric_cols.loc[numeric_cols == True]
-    numeric_cols = numeric_cols.drop(labels=['UID','Duplicate','Categorized At'],axis='index')
-    numeric_cols = numeric_cols.append(pd.Series([True], index=['Upcoming Financial Need After Grants/Scholarships']))
-    col1, _, _ = st.columns(3)
-    with col1:
-        fig_select1a = st.selectbox("Select X axis for graph 1",numeric_cols.index.values)
-        fig_select1b = st.selectbox("Select Y axis for graph 1",numeric_cols.index.values)
-        #sel_rows = grid_table["selected_rows"]
-        #sel_row_indices = [rows['_selectedRowNodeInfo']['nodeRowIndex'] for rows in sel_rows]
-        dynamic_fig(df, fig_select1a, fig_select1b)
+
 
 # Filter selection (Will want to implement this once we have example filters)
 current_filter = st.selectbox("Which filter would you like to apply?",
@@ -73,6 +64,19 @@ grid_table = AgGrid(
     height = 700,
     columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
 )
+
+with st.container():
+    numeric_cols = df.copy().apply(lambda s: pd.to_numeric(s, errors='coerce').notnull().all())
+    numeric_cols = numeric_cols.loc[numeric_cols == True]
+    numeric_cols = numeric_cols.drop(labels=['UID','Duplicate','Categorized At'],axis='index')
+    numeric_cols = numeric_cols.append(pd.Series([True], index=['Upcoming Financial Need After Grants/Scholarships']))
+    col1, _, _ = st.columns(3)
+    with col1:
+        fig_select1a = st.selectbox("Select X axis for graph 1",numeric_cols.index.values)
+        fig_select1b = st.selectbox("Select Y axis for graph 1",numeric_cols.index.values)
+        sel_rows = grid_table["selected_rows"]
+        sel_row_indices = [rows['_selectedRowNodeInfo']['nodeRowIndex'] for rows in sel_rows]
+        dynamic_fig(df, fig_select1a, fig_select1b, sel_row_indices)
 
 # How to access selected rows for use in methods like reviewing
 # sel_rows = grid_table["selected_rows"]
