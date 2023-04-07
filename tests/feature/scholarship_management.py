4251d99e5371188f38b1522f387d8d69d18270ea
Austin Beschen <abeschen@uiowa.edu>
2023-04-07 13:26:15 -0500

    need to fix tests

--- tests/feature/scholarship_management.py
+++ tests/feature/scholarship_management.py
@@ -132,3 +132,23 @@ def test_edit_scholarship(page: Page):
 #     create_sch_btn.click()
 
 #     expect(page.get_by_text("Please make sure all the fields are filled out.")).to_be_visible()
+
+def test_import_page(page: Page):
+    '''
+    As a user I should be able to see the import data section
+    of scholarship management
+    '''
+    page.goto("http://localhost:9000/Scholarship%20Management", wait_until='domcontentloaded')
+
+    import_heading = page.get_by_role("heading", name="Import Scholarships").get_by_text("Import Scholarships")
+
+    expect(import_heading).to_have_text('Import Scholarships')
+
+def test_browse_button(page: Page):
+    '''
+    As a user I should be able to create a new scholarship.
+    '''
+    page.goto("http://localhost:9000/Scholarship%20Management", wait_until='domcontentloaded')
+    browse_btn = page.get_by_role("button", name="Browse files")
+
+    expect(browse_btn).to_have_text('Browse files')
