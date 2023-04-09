d7f7415196873333d9ee22ccb2146560f34ed4bf
Austin Beschen <abeschen@uiowa.edu>
2023-04-09 13:20:33 -0500

    created setUp

--- tests/unit/scholarship_management.py
+++ tests/unit/scholarship_management.py
@@ -3,9 +3,17 @@ Utilities for merging dfs
 '''
 import unittest
 import pandas as pd
-from src.utils.scholarship_management import read_rows, write_rows
+from src.utils.scholarship_management import read_rows, write_rows, edit_row
 
 class ScholarshipManagementTest(unittest.TestCase):
+    def setUp(self):
+        self.scholarships = {'Name': ['Test Scholarship'], 'Total Amount': ['1000'], 'Value': ['2000'], 'RAI': ['315'],
+                           'Admit Score': ['26'], 'Major': ['All'], 'ACT Math': ['25'], 'ACT English': ['27'],
+                           'ACT Composite': ['25'], 'SAT Math': ['500'], 'SAT Reading': ['400'], 'SAT Combined': ['1000'],
+                           'GPA': ['3.75'], 'HS Percentile': ['90'], 'Group One': [['ACT Math', 'SAT Math']],
+                           'Group Two': [['ACT Composite', 'SAT Combined']], 'Group Three': [[]]}
+        self.scholarships_df = pd.DataFrame(self.scholarships)
+    
     def test_read_rows(self):
         '''
         Verify that rows are correctly read from a file
@@ -19,13 +27,7 @@ class ScholarshipManagementTest(unittest.TestCase):
         '''
         Verify that rows are correctly wrote to a file
         '''
-        scholarships = {'Name': ['Test Scholarship'], 'Total Amount': ['1000'], 'Value': ['2000'], 'RAI': ['315'],
-                           'Admit Score': ['26'], 'Major': ['All'], 'ACT Math': ['25'], 'ACT English': ['27'],
-                           'ACT Composite': ['25'], 'SAT Math': ['500'], 'SAT Reading': ['400'], 'SAT Combined': ['1000'],
-                           'GPA': ['3.75'], 'HS Percentile': ['90'], 'Group One': [['ACT Math', 'SAT Math']],
-                           'Group Two': [['ACT Composite', 'SAT Combined']], 'Group Three': [[]]}
-        scholarships_df = pd.DataFrame(scholarships)
-        write_rows(scholarships_df, 'tests/data/scholarships_test_file.xlsx', 'Scholarships')
+        write_rows(self.scholarships_df, 'tests/data/scholarships_test_file.xlsx', 'Scholarships')
         
         rows = read_rows('tests/data/scholarships_test_file.xlsx')
         
@@ -36,8 +38,7 @@ class ScholarshipManagementTest(unittest.TestCase):
 
     # def test_edit_row(self):
     #     return
-    # def test_read_write_rows(self):
-    #     return
+    #     edit_row(rows, 0, [])
     # def test_read_edit_write_rows(self):
     #     return
     # def test_groups_string_to_list(self):
