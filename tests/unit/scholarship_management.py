cce7cd4fa78fb8ae03bb8f23bb0344d367b3f571
Austin Beschen <abeschen@uiowa.edu>
2023-04-09 12:32:44 -0500

    laid out basic test plans

---
+++ tests/unit/scholarship_management.py
@@ -0,0 +1,31 @@
+'''
+Utilities for merging dfs
+'''
+import unittest
+import pandas as pd
+import scholarship_management
+
+class ScholarshipManagementTest(unittest.TestCase):
+    def test_read_rows():
+        return
+    def test_write_rows():
+        return
+    def test_edit_row():
+        return
+    def test_read_write_rows():
+        return
+    def test_read_edit_write_rows():
+        return
+    def test_groups_string_to_list():
+        return
+    def test_display_create_no_error():
+        return
+    def test_display_edit_no_error():
+        return
+    def test_display_delete_no_error():
+        return
+    def test_display_import_no_error():
+        return
+
+if __name__ == '__main__':
+    unittest.main()
