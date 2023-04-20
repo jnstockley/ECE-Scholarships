282b51f7ecda57def520a0a743a5af6d20841e58
Austin Beschen <abeschen@uiowa.edu>
2023-04-20 16:50:28 -0500

    moved old functions

--- src/pages/scholarship_management.py
+++ src/pages/scholarship_management.py
@@ -123,100 +123,6 @@ def display_edit_dynamic():
                 write_rows(scholarships, 'tests/data/scholarships.xlsx', 'Scholarships')
                 st.write(edit_sch + " has been successfully edited.")
 
-# def display_create():
-#     '''
-#     This function displays all of the associated view/actions for creating a scholarship
-#     and adding it to the scholarship file.
-#     '''
-#     st.title('Create a New Scholarship')
-#     st.write('If certain requirements are N/A, leave them at 0.')
-#     # NOTE: text_input is used instead of number_input because I feel like the stepping functionality is unnecessary and could
-#     # throw them off when they see it or they could accidentally hit it. This can be changed later if it is found useful.
-#     name = st.text_input("Scholarship Name", max_chars=500, placeholder="Enter Scholarship Name")
-#     total = st.text_input("Total amount of Scholarships", max_chars=8, placeholder="Enter Numerical Amount")
-#     value = st.text_input('The value of each individual Scholarship', max_chars=8, placeholder="Enter Numerical Amount")
-#     # NOTE: I chose up to 400 for the RAI, but the highest value in the sample data is 363, I am not sure if RAI has a hard max score.
-#     rai = st.select_slider('Select the minimum RAI requirement', options=(x*5 for x in range(0,81)))
-#     # NOTE: I chose up to 36 for this as that is the max in the sample data.
-#     admit_score = st.select_slider('Select the minimum Admit Score requirement', options=range(0,37))
-#     major = st.selectbox("Select which majors the scholarship applies to", options=MAJORS)
-#     act_math = st.select_slider('Select the minimum ACT Math requirement', options=range(0,37))
-#     act_english = st.select_slider('Select the minimum ACT English requirement', options=range(0,37))
-#     act_comp = st.select_slider('Select the minimum ACT Composite requirement', options=range(0,37))
-#     sat_math = st.select_slider('New minimum SAT Math requirement', options=(x*10 for x in range(0,81)))
-#     sat_reading = st.select_slider('New minimum SAT Reading requirement', options=(x*10 for x in range(0,81)))
-#     sat_comb = st.select_slider('Select the minimum SAT Combined requirement', value=(sat_math + sat_reading), options=(x*10 for x in range(0,161)))
-#     # NOTE: 5.0 was chosen as the max for the GPA requirement, but there exists a couple students above 5.0. This could be increased if necessarily,
-#     # but I doubt any scholarships require above 5.0, or even above 4.0, as that would greatly limit students unfairly.
-#     gpa = st.select_slider('Select the minimum GPA requirement', options=(x/20 for x in range (0,101)))
-#     hs_percentile = st.select_slider('Select the minimum highschool percentile', options=(x for x in range(0,101)))
-
-#     group1 = []
-#     group2 = []
-#     group3 = []
-#     if button('Add a Requirement Grouping', key='Add a Requirement Grouping'):
-#         group1 = st.multiselect("Choose Group One", options=GROUP_OPTIONS, help=GROUP_HELP)
-#         if button('Add a second Requirement Grouping', key='Add a second Requirement Grouping'):
-#             group2 = st.multiselect("Choose Group Two", options=GROUP_OPTIONS, help=GROUP_HELP)
-#             if button('Add a third Requirement Grouping', key='Add a third Requirement Grouping'):
-#                 group3 = st.multiselect("Choose Group Three", options=GROUP_OPTIONS, help=GROUP_HELP)
-#     if st.button('Create Scholarship', key='Create Scholarship'):
-#         # These fields should not be able to be blank
-#         if name == "" or total == "" or value == "":
-#             st.write("Please make sure all the fields are filled out.")
-#         else:
-#             # pd.Series creates a Panda object that can be appended to the scholarships dataframe.
-#             scholarship = pd.Series(data=[name, total, value, rai, admit_score, major, act_math, act_english, act_comp, sat_math, sat_reading,
-#                                             sat_comb, gpa, hs_percentile, group1, group2, group3],
-#                                         index=scholarships.columns, name = scholarships.shape[0])
-#             new_scholarships = scholarships.append(scholarship)
-#             write_rows(new_scholarships, 'tests/data/scholarships.xlsx', 'Scholarships')
-#             st.write(name + " has been successfully created.")
-
-# def display_edit():
-#     '''
-#     This function displays all of the associated view/actions for editing a scholarship
-#     and overwriting the old version with the new version in the scholarship file.
-#     '''
-#     edit_sch = st.selectbox("Select the scholarship to edit", options=scholarships['Name'])
-#     if button('Edit This Scholarship', key = 'Edit This Scholarship'):
-#         # Don't let them try to edit nothing.
-#         if edit_sch is None:
-#             st.write('There is no scholarship selected')
-#         else:
-#             # values is the current values of the scholarship, index is the row of that scholarship
-#             # Find both the index and the values that match the scholarship we are trying to edit.
-#             for ind in range(0, scholarships.shape[0]):
-#                 values = scholarships.iloc[ind]
-#                 if values['Name'] == edit_sch:
-#                     index = ind
-#                     break
-#             # Display the information; value is set so that it loads the current values into the fields.
-#             total = st.text_input("New total amount of Scholarships", value = values['Total Amount'], max_chars=8, placeholder="Enter Numerical Amount")
-#             value = st.text_input('New value of each individual Scholarship', value = values['Value'], max_chars=8, placeholder="Enter Numerical Amount")
-#             rai = st.select_slider('Select the minimum RAI requirement', value=values['RAI'], options=(x*5 for x in range(0,81)))
-#             admit_score = st.select_slider('Select the minimum Admit Score requirement', value=values['Admit Score'], options=range(0,37))
-#             major = st.selectbox("New majors the scholarship applies to", index = MAJORS.index(values['Major']), options=MAJORS)
-#             act_math = st.select_slider('Select the minimum ACT Math requirement', value=values['ACT Math'], options=range(0,37))
-#             act_english = st.select_slider('Select the minimum ACT English requirement', value=values['ACT English'], options=range(0,37))
-#             act_comp = st.select_slider('New minimum ACT Composite requirement', value=values['ACT Composite'], options=range(0,37))
-#             sat_math = st.select_slider('New minimum SAT Math requirement', value=values['SAT Math'], options=(x*10 for x in range(0,81)))
-#             sat_reading = st.select_slider('New minimum SAT Reading requirement', value=values['SAT Reading'], options=(x*10 for x in range(0,81)))
-#             sat_comb = st.select_slider('Select the minimum SAT Combined requirement', value=values['SAT Combined'], options=(x*10 for x in range(0,161)))
-#             gpa = st.select_slider('New minimum GPA requirement', value=values['GPA'], options=(x/20 for x in range (0,101)))
-#             hs_percentile = st.select_slider('Select the minimum highschool percentile', value=values['HS Percentile'], options=(x for x in range(0,101)))
-#             group1 = st.multiselect("Choose Group One", options=GROUP_OPTIONS, default=groups_string_to_list(values['Group One']), help=GROUP_HELP)
-#             group2 = st.multiselect("Choose Group Two", options=GROUP_OPTIONS, default=groups_string_to_list(values['Group Two']), help=GROUP_HELP)
-#             group3 = st.multiselect("Choose Group Three", options=GROUP_OPTIONS, default=groups_string_to_list(values['Group Three']), help=GROUP_HELP)
-#             if st.button('Finalize Changes', key='Finalize Changes'):
-#                 edit_row(scholarships, index, [('Total Amount', total), ('Value', value), ('RAI', rai), ('Admit Score', admit_score),('Major', major),
-#                                                ('ACT Math', act_math), ('ACT English', act_english), ('ACT Composite', act_comp),('SAT Math', sat_math),
-#                                                ('SAT Reading', sat_reading), ('SAT Combined', sat_comb), ('GPA', gpa),('HS Percentile', hs_percentile),
-#                                                ('Group One', str(group1)), ('Group Two', str(group2)), ('Group Three', str(group3))])
-#                 # We changed the values in our scholarships dataframe, but have not updated the actual file, so that is done here
-#                 write_rows(scholarships, 'tests/data/scholarships.xlsx', 'Scholarships')
-#                 st.write(edit_sch + " has been successfully edited.")
-
 def display_delete():
     '''
     This function displays all of the associated view/actions for deleting a scholarship
@@ -329,3 +235,101 @@ with st.container():
         display_delete()
 
 display_import()
+
+
+
+# NOTE: Deprecated Functions that are kept in case they are needed.
+
+# def display_create():
+#     '''
+#     This function displays all of the associated view/actions for creating a scholarship
+#     and adding it to the scholarship file.
+#     '''
+#     st.title('Create a New Scholarship')
+#     st.write('If certain requirements are N/A, leave them at 0.')
+#     # NOTE: text_input is used instead of number_input because I feel like the stepping functionality is unnecessary and could
+#     # throw them off when they see it or they could accidentally hit it. This can be changed later if it is found useful.
+#     name = st.text_input("Scholarship Name", max_chars=500, placeholder="Enter Scholarship Name")
+#     total = st.text_input("Total amount of Scholarships", max_chars=8, placeholder="Enter Numerical Amount")
+#     value = st.text_input('The value of each individual Scholarship', max_chars=8, placeholder="Enter Numerical Amount")
+#     # NOTE: I chose up to 400 for the RAI, but the highest value in the sample data is 363, I am not sure if RAI has a hard max score.
+#     rai = st.select_slider('Select the minimum RAI requirement', options=(x*5 for x in range(0,81)))
+#     # NOTE: I chose up to 36 for this as that is the max in the sample data.
+#     admit_score = st.select_slider('Select the minimum Admit Score requirement', options=range(0,37))
+#     major = st.selectbox("Select which majors the scholarship applies to", options=MAJORS)
+#     act_math = st.select_slider('Select the minimum ACT Math requirement', options=range(0,37))
+#     act_english = st.select_slider('Select the minimum ACT English requirement', options=range(0,37))
+#     act_comp = st.select_slider('Select the minimum ACT Composite requirement', options=range(0,37))
+#     sat_math = st.select_slider('New minimum SAT Math requirement', options=(x*10 for x in range(0,81)))
+#     sat_reading = st.select_slider('New minimum SAT Reading requirement', options=(x*10 for x in range(0,81)))
+#     sat_comb = st.select_slider('Select the minimum SAT Combined requirement', value=(sat_math + sat_reading), options=(x*10 for x in range(0,161)))
+#     # NOTE: 5.0 was chosen as the max for the GPA requirement, but there exists a couple students above 5.0. This could be increased if necessarily,
+#     # but I doubt any scholarships require above 5.0, or even above 4.0, as that would greatly limit students unfairly.
+#     gpa = st.select_slider('Select the minimum GPA requirement', options=(x/20 for x in range (0,101)))
+#     hs_percentile = st.select_slider('Select the minimum highschool percentile', options=(x for x in range(0,101)))
+
+#     group1 = []
+#     group2 = []
+#     group3 = []
+#     if button('Add a Requirement Grouping', key='Add a Requirement Grouping'):
+#         group1 = st.multiselect("Choose Group One", options=GROUP_OPTIONS, help=GROUP_HELP)
+#         if button('Add a second Requirement Grouping', key='Add a second Requirement Grouping'):
+#             group2 = st.multiselect("Choose Group Two", options=GROUP_OPTIONS, help=GROUP_HELP)
+#             if button('Add a third Requirement Grouping', key='Add a third Requirement Grouping'):
+#                 group3 = st.multiselect("Choose Group Three", options=GROUP_OPTIONS, help=GROUP_HELP)
+#     if st.button('Create Scholarship', key='Create Scholarship'):
+#         # These fields should not be able to be blank
+#         if name == "" or total == "" or value == "":
+#             st.write("Please make sure all the fields are filled out.")
+#         else:
+#             # pd.Series creates a Panda object that can be appended to the scholarships dataframe.
+#             scholarship = pd.Series(data=[name, total, value, rai, admit_score, major, act_math, act_english, act_comp, sat_math, sat_reading,
+#                                             sat_comb, gpa, hs_percentile, group1, group2, group3],
+#                                         index=scholarships.columns, name = scholarships.shape[0])
+#             new_scholarships = scholarships.append(scholarship)
+#             write_rows(new_scholarships, 'tests/data/scholarships.xlsx', 'Scholarships')
+#             st.write(name + " has been successfully created.")
+
+# def display_edit():
+#     '''
+#     This function displays all of the associated view/actions for editing a scholarship
+#     and overwriting the old version with the new version in the scholarship file.
+#     '''
+#     edit_sch = st.selectbox("Select the scholarship to edit", options=scholarships['Name'])
+#     if button('Edit This Scholarship', key = 'Edit This Scholarship'):
+#         # Don't let them try to edit nothing.
+#         if edit_sch is None:
+#             st.write('There is no scholarship selected')
+#         else:
+#             # values is the current values of the scholarship, index is the row of that scholarship
+#             # Find both the index and the values that match the scholarship we are trying to edit.
+#             for ind in range(0, scholarships.shape[0]):
+#                 values = scholarships.iloc[ind]
+#                 if values['Name'] == edit_sch:
+#                     index = ind
+#                     break
+#             # Display the information; value is set so that it loads the current values into the fields.
+#             total = st.text_input("New total amount of Scholarships", value = values['Total Amount'], max_chars=8, placeholder="Enter Numerical Amount")
+#             value = st.text_input('New value of each individual Scholarship', value = values['Value'], max_chars=8, placeholder="Enter Numerical Amount")
+#             rai = st.select_slider('Select the minimum RAI requirement', value=values['RAI'], options=(x*5 for x in range(0,81)))
+#             admit_score = st.select_slider('Select the minimum Admit Score requirement', value=values['Admit Score'], options=range(0,37))
+#             major = st.selectbox("New majors the scholarship applies to", index = MAJORS.index(values['Major']), options=MAJORS)
+#             act_math = st.select_slider('Select the minimum ACT Math requirement', value=values['ACT Math'], options=range(0,37))
+#             act_english = st.select_slider('Select the minimum ACT English requirement', value=values['ACT English'], options=range(0,37))
+#             act_comp = st.select_slider('New minimum ACT Composite requirement', value=values['ACT Composite'], options=range(0,37))
+#             sat_math = st.select_slider('New minimum SAT Math requirement', value=values['SAT Math'], options=(x*10 for x in range(0,81)))
+#             sat_reading = st.select_slider('New minimum SAT Reading requirement', value=values['SAT Reading'], options=(x*10 for x in range(0,81)))
+#             sat_comb = st.select_slider('Select the minimum SAT Combined requirement', value=values['SAT Combined'], options=(x*10 for x in range(0,161)))
+#             gpa = st.select_slider('New minimum GPA requirement', value=values['GPA'], options=(x/20 for x in range (0,101)))
+#             hs_percentile = st.select_slider('Select the minimum highschool percentile', value=values['HS Percentile'], options=(x for x in range(0,101)))
+#             group1 = st.multiselect("Choose Group One", options=GROUP_OPTIONS, default=groups_string_to_list(values['Group One']), help=GROUP_HELP)
+#             group2 = st.multiselect("Choose Group Two", options=GROUP_OPTIONS, default=groups_string_to_list(values['Group Two']), help=GROUP_HELP)
+#             group3 = st.multiselect("Choose Group Three", options=GROUP_OPTIONS, default=groups_string_to_list(values['Group Three']), help=GROUP_HELP)
+#             if st.button('Finalize Changes', key='Finalize Changes'):
+#                 edit_row(scholarships, index, [('Total Amount', total), ('Value', value), ('RAI', rai), ('Admit Score', admit_score),('Major', major),
+#                                                ('ACT Math', act_math), ('ACT English', act_english), ('ACT Composite', act_comp),('SAT Math', sat_math),
+#                                                ('SAT Reading', sat_reading), ('SAT Combined', sat_comb), ('GPA', gpa),('HS Percentile', hs_percentile),
+#                                                ('Group One', str(group1)), ('Group Two', str(group2)), ('Group Three', str(group3))])
+#                 # We changed the values in our scholarships dataframe, but have not updated the actual file, so that is done here
+#                 write_rows(scholarships, 'tests/data/scholarships.xlsx', 'Scholarships')
+#                 st.write(edit_sch + " has been successfully edited.")
