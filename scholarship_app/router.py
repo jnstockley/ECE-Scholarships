003b6f77f763476a9ca1a5ad0e9b579f0fb947ee
Brandon Egger <brandonegger64@gmail.com>
2023-04-26 13:52:59 -0500

    use relative project mapping

--- scholarship_app/router.py
+++ scholarship_app/router.py
@@ -1,16 +1,19 @@
 '''
 Main entry point for streamlit app. Sets up the page routing for the application.
 '''
+import pathlib
 from st_pages import Page, show_pages
+
+HERE = pathlib.Path(__file__).parent
 
 show_pages(
     [
-        Page("scholarship_app/pages/home.py", "Home"),
-        Page("scholarship_app/pages/scholarship_management.py", "Scholarship Management"),
-        Page("scholarship_app/pages/login.py", "Log In"),
-        Page("scholarship_app/pages/download.py", "Download File"),
-        Page("scholarship_app/pages/student_metrics.py", "Student Metrics"),
-        Page("scholarship_app/pages/import.py", "Import Data"),
-        Page("scholarship_app/pages/export.py", "Export Data"),
+        Page(HERE.joinpath("pages/home.py"), "Home"),
+        Page(HERE.joinpath("pages/scholarship_management.py"), "Scholarship Management"),
+        Page(HERE.joinpath("pages/login.py"), "Log In"),
+        Page(HERE.joinpath("pages/download.py"), "Download File"),
+        Page(HERE.joinpath("pages/student_metrics.py"), "Student Metrics"),
+        Page(HERE.joinpath("pages/import.py"), "Import Data"),
+        Page(HERE.joinpath("pages/export.py"), "Export Data"),
     ]
 )
