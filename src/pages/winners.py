ae7953e71b8db678ecd3e6853012d499c1c0f0ea
Evan Lemker <lemkere@gmail.com>
2023-05-02 17:19:27 -0500

    Cleaned up code in Winners

--- src/pages/winners.py
+++ src/pages/winners.py
@@ -1,31 +1,31 @@
 # Importing packages
-# Packages used in code
 import os
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 from src.utils.html import redirect
 from src.utils.sharepoint import logged_in, download, login, get_files
 
 # Default setting for Streamlit page
 st.set_page_config(layout="wide")
 
-# Log in protecting the home page
+# Log in protecting the page
 cookie = logged_in()
-
 if not cookie:
    redirect("/Log In")
 
+# Downloading the data needed on first visit
 @st.cache_data
 def download_data():
-
+    '''
+    Caching credentials and downloads so only have to do on page load
+    '''
+    # Gathering login credentials
     creds = login(cookie)
-
     hawk_id = cookie.get('cred')['hawk-id']
 
+    # Downloading needed data
     files = get_files(creds)
-
     for file in files: 
         if file == "Select File":
             continue
@@ -33,7 +33,7 @@ def download_data():
             if '/data/' in file:
                 download(file, f"{os.getcwd()}/data/", creds)
     
-    # Initialize data: 
+    # Initializing session data
     st.session_state.students = pd.read_excel("./data/Master_Sheet.xlsx")
     st.session_state.scholarships = pd.read_excel("./data/Scholarships.xlsx")
     directory = "./data"
@@ -46,28 +46,22 @@ def download_data():
     st.session_state.all_recommendations = result
 
     return creds, hawk_id
-    
+
+# Setting variables for script
 creds, hawk_id = download_data()
-
-
-# Setting variables
-
 students = st.session_state.students
-
+current_data = students.copy()
 scholarships = st.session_state.scholarships
-
 all_recommendations = st.session_state.all_recommendations
 
 # Start of display
 st.header("Export Scholarship Winners")
 
-# Filter selection (Will want to implement this once we have example filters)
+# Scholarhsip selection
 current_scholarship = st.selectbox("Which scholarship would you like to consider?", np.append(["None"], scholarships["Name"].values))
 
-current_data = students.copy()
+# Calculating vote scores for selected scholarship
 current_data.insert(0, 'Vote Score', None)
-
-# Calculating the vote score across recommendations
 for student_index, student in current_data.iterrows():
     for recommender in all_recommendations:
         for recommendation_index, recommendation in recommender.iterrows():
@@ -86,20 +80,28 @@ current_data = current_data[current_data['Vote Score'].notna()]
 current_data.drop(current_data.loc[current_data['Vote Score'] < 0].index, inplace = True)
 current_data = current_data.sort_values(by=['Vote Score'], ascending=False)
 
+# Giving feedback if no scholarship selected
 if current_scholarship == "None":
     st.error("Select a Scholarship")
 
+# Displaying students that received a vote score >= 0
 st.write(current_data)
 
+# Actions for user to take on current data
 with st.container(): 
     col1, col2, col3 = st.columns(3)
 
+    # Export all students that received a vote score >= 0
     with col1: 
         if st.button("Export All Vote Getters for Scholarship"):
             current_data.to_excel('./data/Scholarship_Winners.xlsx')
             st.success('Exported data to /data! as Scholarship_Winners.xlsx')
+
+    # Choose a number of top vote scorers to export
     with col2: 
         value = st.number_input('Number to export', min_value=0, step=1, label_visibility='collapsed')
+    
+    # Export the previously chosen number of top vote scorers 
     with col3: 
          if st.button(f"Export Top {value} Vote Getters for Scholarship"):
             current_data.iloc[:value].to_excel('./data/Scholarship_Winners.xlsx')
