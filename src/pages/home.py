44c57918bbde379ca8a06d57280f5050117eb665
Ashelyn Mann <ashem@uiowa.edu>
2023-05-02 16:12:17 -0500

    fixing linting

--- src/pages/home.py
+++ src/pages/home.py
@@ -54,10 +54,14 @@ CLEARJS = '''<script>
 st.title("Home")
 st.header("Review Applicants")
 
-def dynamic_fig(var_df, x_axis, y_axis, show_legend = False, weight_bins=True, highlight_select = 'None', highlights=None):
+def dynamic_fig(var_df, x_axis, y_axis, options=None, highlights=None):
     '''
     Function to generate dynamic graph of student data
     '''
+    show_legend = options[0]
+    weight_bins = options[1]
+    highlight_select = options[2]
+
     fig, axis = plt.subplots()
     var_xs = var_df[x_axis][var_df[x_axis] != 0][var_df[y_axis] != 0]
     var_ys = var_df[y_axis][var_df[x_axis] != 0][var_df[y_axis] != 0]
@@ -73,17 +77,16 @@ def dynamic_fig(var_df, x_axis, y_axis, show_legend = False, weight_bins=True, h
                 weighted_bins[j][2] += 1
                 found = True
     weighted_bins = weighted_bins[~np.all(weighted_bins == 0, axis=1)]
-    for bin in weighted_bins:
+    for wbin in weighted_bins:
         if weight_bins:
-            bin[-1] = bin[-1] - (np.min(weighted_bins[:,2])-1)
+            wbin[-1] = wbin[-1] - (np.min(weighted_bins[:,2])-1)
         else:
-            bin[-1] = 1
-        if bin[-1] > 10:
-            bin[-1] = 10
+            wbin[-1] = 1
+        if wbin[-1] > 10:
+            wbin[-1] = 10
     plt.scatter(weighted_bins[:,0], weighted_bins[:,1], s=32*weighted_bins[:,2])
     if highlights is not None and highlight_select == 'Selected Students':
         highlights = [h for h in highlights if h is not None]
-        print(highlights)
         hxs = var_df.iloc[highlights][x_axis]
         hys = var_df.iloc[highlights][y_axis]
         colors = iter(cm.rainbow(np.linspace(0, 1, len(hys)+1)))
@@ -190,11 +193,11 @@ with st.container():
                 fig_select1c = st.selectbox("Highlight Scheme", ['None', 'Selected Students'])#, 'Scholarship Status'
                 show_legend = st.checkbox("Show Legend", True)
                 weight_bins = st.checkbox("Weight Plot", True)
+                sel_row_indices = None
                 if fig_select1c == 'Selected Students':
                     sel_rows = grid_table["selected_rows"]
                     sel_row_indices = [rows['_selectedRowNodeInfo']['nodeRowIndex'] for rows in sel_rows]
-                if fig_select1c == 'None':
-                    sel_row_indices = None
-                dynamic_fig(STUDENTS, fig_select1a, fig_select1b, show_legend, weight_bins, fig_select1c, sel_row_indices)    # Exporting the selected students
+                options = [show_legend, weight_bins, fig_select1c]
+                dynamic_fig(STUDENTS, fig_select1a, fig_select1b, options, sel_row_indices)    # Exporting the selected students
     with col3:
         st.button("Export Selected Students")
