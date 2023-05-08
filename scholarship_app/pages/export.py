ffba7857b79e2bfdc116d4f132e590bf107b83db
Brandon Egger <brandonegger64@gmail.com>
2023-05-07 19:30:42 -0500

    add imported data as resource from sharepoint

--- scholarship_app/pages/export.py
+++ scholarship_app/pages/export.py
@@ -2,7 +2,6 @@
 Export data page
 """
 import streamlit as st
-from scholarship_app.sessions.session_manager import SessionManager
 from scholarship_app.managers.sharepoint.sharepoint_session import SharepointSession
 from scholarship_app.utils.output import get_appdata_path
 from scholarship_app.managers.sharepoint.file_versioning import DataManager, DataType
@@ -19,13 +18,17 @@ with st.spinner("Downloading Data..."):
     main_data = MAIN_DATA.retrieve_master()
 
 if main_data is None:
-    st.write("No master data imported, once you import data and save it to the master sheet in sharepoint you can return to export data and download that sheet at any point")
+    st.write(
+        "No master data imported, once you import data and save it to the master sheet in sharepoint you can return to export data and download that sheet at any point"
+    )
 else:
     st.write("Download your sharepoint master datasheet")
 
     main_data.to_excel(f"{get_appdata_path('temp/download')}/student_data_export.xls")
 
-    with open(f"{get_appdata_path('temp/download')}/student_data_export.xls", "rb") as file:
+    with open(
+        f"{get_appdata_path('temp/download')}/student_data_export.xls", "rb"
+    ) as file:
         st.download_button(
             label="Export",
             data=file,
